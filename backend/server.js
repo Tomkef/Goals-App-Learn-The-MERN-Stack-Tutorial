d518f0423b8cce8578f32f9a7dfdd653d3df2de4
Tomkef <tomkef@gmail.com>
2022-04-20 21:42:00 +0300

    Added serving of frontend files on production

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,9 @@ const colors = require("colors");
 const { errorHandler } = require("./middleware/errorMiddleware");
 const dotenv = require("dotenv").config();
 const connectDB = require("./config/db");
+const path = require("path");
+const { rmSync } = require("fs");
+
 const port = process.env.PORT || 5000;
 
 connectDB();
@@ -14,6 +17,20 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use(`/api/goals`, require("./routes/goalRoutes"));
 app.use(`/api/users`, require("./routes/userRoutes"));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+  app.get("*", (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, "../", "frontend", "build", "index.html")
+    )
+  );
+}
+/*
+else {
+  app.get("/", (req, res) => res.send("Please set to production"));
+}
+*/
 
 app.use(errorHandler);
 
